0384e6a8ee87387275fc2cb52dbf848ab44a0583
Juan Muñoz <alexx44@users.noreply.github.com>
2016-11-16 17:46:54 -0300

    Se incluyen los archivos y controladores de login
    ademas se agrega el contenido de app.css a main.css

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reservasApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reservasApp
+ */
+angular.module('reservasApp')
+  .controller('login', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
